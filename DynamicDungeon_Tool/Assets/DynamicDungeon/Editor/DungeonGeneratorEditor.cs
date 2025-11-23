a21f1ce54225dc82a9cd202ae8454c30a0e465a2
MrBytesized <williamlgale@gmail.com>
2025-11-23 23:00:19 +0000

    feat(editor): implement automatic game object creation from create menu

--- DynamicDungeon_Tool/Assets/DynamicDungeon/Editor/DungeonGeneratorEditor.cs
+++ DynamicDungeon_Tool/Assets/DynamicDungeon/Editor/DungeonGeneratorEditor.cs
@@ -6,14 +6,13 @@ public class DungeonGeneratorEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        DrawDefaultInspector(); // Draws the normal Width/Height/Tile fields
+        DrawDefaultInspector(); 
 
         DungeonGenerator generator = (DungeonGenerator)target;
 
         GUILayout.Space(10);
 
-        // The Big "Generate" Button
-        if (GUILayout.Button("Generate Map (P1 Test)", GUILayout.Height(40)))
+        if (GUILayout.Button("Generate Map", GUILayout.Height(40)))
         {
             generator.GenerateEmptyMap(generator.width, generator.height);
         }
