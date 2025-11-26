9411794e8ed30de23aceccfdb2ce7395c3708da3
MrBytesized <williamlgale@gmail.com>
2025-11-26 01:19:09 +0000

    feat(demo): add width controls, height controls and zoom controls to on screen UI

--- DynamicDungeon_Tool/Assets/TileMapGeneratorDemoUI.cs
+++ DynamicDungeon_Tool/Assets/TileMapGeneratorDemoUI.cs
@@ -4,7 +4,9 @@ using UnityEngine.UI;
 
 public class TileMapGeneratorDemoUI : MonoBehaviour
 {
-
+    [SerializeField] TMP_InputField widthInputField;
+    [SerializeField] TMP_InputField heightInputField;
+    [SerializeField] Toggle fillToScreenSize;
     [SerializeField] Button generateButton;
     [SerializeField] Button clearButton;
     [SerializeField] Slider randomFillPercentageSlider;
@@ -14,6 +16,9 @@ public class TileMapGeneratorDemoUI : MonoBehaviour
     [SerializeField] Toggle useBorderWallsToggle;
     [SerializeField] TMP_InputField seedInputField;
     [SerializeField] Toggle useRandomSeedToggle;
+    [SerializeField] Slider mapZoomSlider;
+    [SerializeField] Button resetCameraButton;
+    [SerializeField] Camera mapCamera;
     [SerializeField] TilemapGenerator tileMapGenerator;
 
     void Start()
@@ -22,10 +27,74 @@ public class TileMapGeneratorDemoUI : MonoBehaviour
         randomFillPercentageValueText.text = $"Random Fill {tileMapGenerator.randomFillPercent}%";
         smoothingIterationsSlider.value = (float)tileMapGenerator.smoothIterations / 10f;
         smoothingIterationsValueText.text = $"Smoothing Iterations {tileMapGenerator.smoothIterations}";
+        fillToScreenSize.isOn = false;
         useBorderWallsToggle.isOn = tileMapGenerator.useBorderWalls;
         seedInputField.text = tileMapGenerator.seed;
         useRandomSeedToggle.isOn = tileMapGenerator.useRandomSeed;
         seedInputField.interactable = !tileMapGenerator.useRandomSeed;
+        widthInputField.text = tileMapGenerator.width.ToString();
+        heightInputField.text = tileMapGenerator.height.ToString();
+        mapZoomSlider.value = (mapCamera.orthographicSize - 5f) / (50f - 5f);
+
+        void UpdateMapSizeToCamera()
+        {
+            float camHeight = mapCamera.orthographicSize * 2f;
+            float camWidth = camHeight * mapCamera.aspect;
+
+            int screenWidthInTiles = Mathf.CeilToInt(camWidth);
+            int screenHeightInTiles = Mathf.CeilToInt(camHeight);
+
+            tileMapGenerator.width = screenWidthInTiles;
+            tileMapGenerator.height = screenHeightInTiles;
+            widthInputField.text = screenWidthInTiles.ToString();
+            heightInputField.text = screenHeightInTiles.ToString();
+        }
+
+        fillToScreenSize.onValueChanged.AddListener((isOn) =>
+        {
+            if (isOn)
+            {
+                UpdateMapSizeToCamera();
+                widthInputField.interactable = false;
+                heightInputField.interactable = false;
+            }
+            else
+            {
+                widthInputField.interactable = true;
+                heightInputField.interactable = true;
+            }
+        });
+
+        widthInputField.onValueChanged.AddListener((value) =>
+        {
+            if (int.TryParse(value, out int width))
+            {
+                tileMapGenerator.width = Mathf.Max(1, width);
+            }
+        });
+
+        heightInputField.onValueChanged.AddListener((value) =>
+        {
+            if (int.TryParse(value, out int height))
+            {
+                tileMapGenerator.height = Mathf.Max(1, height);
+            }
+        });
+
+        mapZoomSlider.onValueChanged.AddListener((value) =>
+        {
+            mapCamera.orthographicSize = Mathf.Lerp(5f, 50f, value);
+            if (fillToScreenSize.isOn)
+            {
+                UpdateMapSizeToCamera();
+            }
+        });
+
+        resetCameraButton.onClick.AddListener(() =>
+        {
+            mapCamera.orthographicSize = 5f;
+            mapZoomSlider.value = (20f - 5f) / (50f - 5f);
+        });
 
         seedInputField.onValueChanged.AddListener((value) =>
         {
