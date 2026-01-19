de9d3c14e984450007b5affed9cdcfb9c4e255bc
MrBytesized <williamlgale@gmail.com>
2026-01-19 00:56:07 +0000

    refactor(editor): embed RegionSettings and Profile inspectors into main generator editor

--- DynamicDungeon_Tool/Assets/DynamicDungeon/Editor/DungeonGeneratorEditor.cs
+++ DynamicDungeon_Tool/Assets/DynamicDungeon/Editor/DungeonGeneratorEditor.cs
@@ -7,16 +7,94 @@ public class DungeonGeneratorEditor : Editor
     private Texture2D _previewTexture;
     private bool _showRegionPreview = true;
 
+    private Editor _regionSettingsEditor;
+    private Editor _generationProfileEditor;
+    private bool _showRegionSettings = true;
+    private bool _showProfileSettings = false;
+
+    private void OnEnable()
+    {
+        TilemapGenerator generator = (TilemapGenerator)target;
+        UpdateEditors(generator);
+    }
+
+    private void OnDisable()
+    {
+        if (_regionSettingsEditor != null) DestroyImmediate(_regionSettingsEditor);
+        if (_generationProfileEditor != null) DestroyImmediate(_generationProfileEditor);
+    }
+
     public override void OnInspectorGUI()
     {
-        DrawDefaultInspector();
-
         TilemapGenerator generator = (TilemapGenerator)target;
+
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("tilemap"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("width"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("height"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("useBorderWalls"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("seed"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("useRandomSeed"));
+
+        SerializedProperty regionProp = serializedObject.FindProperty("regionSettings");
+
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(regionProp);
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+            UpdateEditors(generator);
+        }
+
+        if (generator.regionSettings != null)
+        {
+            _showRegionSettings = EditorGUILayout.Foldout(_showRegionSettings, "Region Settings (Quick Edit)", true, EditorStyles.foldoutHeader);
+            if (_showRegionSettings)
+            {
+                EditorGUI.indentLevel++;
+
+                if (_regionSettingsEditor == null) UpdateEditors(generator);
+
+                EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                _regionSettingsEditor.OnInspectorGUI();
+                EditorGUILayout.EndVertical();
+
+                EditorGUI.indentLevel--;
+            }
+        }
+
+        SerializedProperty profileProp = serializedObject.FindProperty("generationProfile");
+
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(profileProp);
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+            UpdateEditors(generator);
+        }
+
+        if (generator.generationProfile != null)
+        {
+            _showProfileSettings = EditorGUILayout.Foldout(_showProfileSettings, "Generation Profile (Quick Edit)", true, EditorStyles.foldoutHeader);
+            if (_showProfileSettings)
+            {
+                EditorGUI.indentLevel++;
+                if (_generationProfileEditor == null) UpdateEditors(generator);
+
+                EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                _generationProfileEditor.OnInspectorGUI();
+                EditorGUILayout.EndVertical();
+                EditorGUI.indentLevel--;
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
 
         GUILayout.Space(15);
         GUILayout.Label("Development Tools", EditorStyles.boldLabel);
 
-        _showRegionPreview = EditorGUILayout.Foldout(_showRegionPreview, "Region Map Preview");
+        _showRegionPreview = EditorGUILayout.Foldout(_showRegionPreview, "Region Map Preview", true);
         if (_showRegionPreview && generator.CurrentRegionMap != null && generator.regionSettings != null)
         {
             if (_previewTexture == null || _previewTexture.width != generator.width || _previewTexture.height != generator.height)
@@ -26,6 +104,12 @@ public class DungeonGeneratorEditor : Editor
 
             Rect rect = GUILayoutUtility.GetRect(200, 200, GUILayout.ExpandWidth(true));
             EditorGUI.DrawPreviewTexture(rect, _previewTexture, null, ScaleMode.ScaleToFit);
+
+            if (GUILayout.Button("Refresh Preview Only"))
+            {
+                generator.GenerateTilemap();
+                UpdatePreview(generator);
+            }
         }
 
         GUILayout.Space(10);
@@ -37,7 +121,7 @@ public class DungeonGeneratorEditor : Editor
         {
             if (generator.tilemap != null) Undo.RecordObject(generator.tilemap, generateLabel);
             generator.GenerateTilemap();
-            UpdatePreview(generator); 
+            UpdatePreview(generator);
             Repaint();
         }
 
@@ -53,6 +137,37 @@ public class DungeonGeneratorEditor : Editor
                 generator.ClearGeneratedMap();
                 _previewTexture = null;
                 Repaint();
+            }
+        }
+    }
+
+    private void UpdateEditors(TilemapGenerator generator)
+    {
+        if (generator.regionSettings != null)
+        {
+            if (_regionSettingsEditor != null && _regionSettingsEditor.target != generator.regionSettings)
+            {
+                DestroyImmediate(_regionSettingsEditor);
+                _regionSettingsEditor = null;
+            }
+
+            if (_regionSettingsEditor == null)
+            {
+                _regionSettingsEditor = CreateEditor(generator.regionSettings);
+            }
+        }
+
+        if (generator.generationProfile != null)
+        {
+            if (_generationProfileEditor != null && _generationProfileEditor.target != generator.generationProfile)
+            {
+                DestroyImmediate(_generationProfileEditor);
+                _generationProfileEditor = null;
+            }
+
+            if (_generationProfileEditor == null)
+            {
+                _generationProfileEditor = CreateEditor(generator.generationProfile);
             }
         }
     }
@@ -78,7 +193,6 @@ public class DungeonGeneratorEditor : Editor
             for (int y = 0; y < h; y++)
             {
                 int biomeIdx = generator.CurrentRegionMap[x, y];
-
                 if (generator.regionSettings != null && biomeIdx < generator.regionSettings.biomes.Count)
                 {
                     pixels[y * w + x] = generator.regionSettings.biomes[biomeIdx].biome.debugColor;
---
+++ DynamicDungeon_Tool/Assets/DynamicDungeon/Editor/RegionSettingsEditor.cs
@@ -0,0 +1,143 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(RegionSettings))]
+public class RegionSettingsEditor : Editor
+{
+    private SerializedProperty _biomesProp;
+    private SerializedProperty _algorithmProp;
+
+    private SerializedProperty _voronoiNumSites;
+    private SerializedProperty _perlinScale;
+
+    private Dictionary<string, Editor> _biomeEditors = new Dictionary<string, Editor>();
+
+    private void OnEnable()
+    {
+        _biomesProp = serializedObject.FindProperty("biomes");
+        _algorithmProp = serializedObject.FindProperty("algorithm");
+
+        _voronoiNumSites = serializedObject.FindProperty("voronoiNumSites");
+        _perlinScale = serializedObject.FindProperty("perlinScale");
+    }
+
+    private void OnDisable()
+    {
+        foreach (Editor editor in _biomeEditors.Values)
+        {
+            if (editor != null) DestroyImmediate(editor);
+        }
+        _biomeEditors.Clear();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(_algorithmProp);
+
+        RegionAlgorithm algo = (RegionAlgorithm)_algorithmProp.enumValueIndex;
+
+        EditorGUILayout.Space(5);
+        if (algo == RegionAlgorithm.Voronoi)
+        {
+            EditorGUILayout.LabelField("Voronoi Configuration", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(_voronoiNumSites);
+        }
+        else if (algo == RegionAlgorithm.PerlinNoise)
+        {
+            EditorGUILayout.LabelField("Perlin Configuration", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(_perlinScale);
+        }
+
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Biome Palette Configuration", EditorStyles.boldLabel);
+
+        DrawBiomesList();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void DrawBiomesList()
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label($"Biomes Defined: {_biomesProp.arraySize}", EditorStyles.miniLabel);
+        if (GUILayout.Button("+ Add Biome", GUILayout.Width(100)))
+        {
+            _biomesProp.InsertArrayElementAtIndex(_biomesProp.arraySize);
+            SerializedProperty newItem = _biomesProp.GetArrayElementAtIndex(_biomesProp.arraySize - 1);
+            newItem.FindPropertyRelative("weight").intValue = 10;
+        }
+        EditorGUILayout.EndHorizontal();
+
+        for (int i = 0; i < _biomesProp.arraySize; i++)
+        {
+            SerializedProperty element = _biomesProp.GetArrayElementAtIndex(i);
+            SerializedProperty biomeRef = element.FindPropertyRelative("biome");
+            SerializedProperty weight = element.FindPropertyRelative("weight");
+
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.LabelField("Weight:", GUILayout.Width(75));
+
+            int w = EditorGUILayout.IntField(weight.intValue, GUILayout.Width(50));
+            if (w < 1) w = 1;
+            weight.intValue = w;
+
+            EditorGUILayout.PropertyField(biomeRef, GUIContent.none);
+
+            if (GUILayout.Button("X", GUILayout.Width(25)))
+            {
+                _biomesProp.DeleteArrayElementAtIndex(i);
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.EndVertical();
+                break;
+            }
+            EditorGUILayout.EndHorizontal();
+
+            if (biomeRef.objectReferenceValue != null)
+            {
+                BiomeData biomeData = (BiomeData)biomeRef.objectReferenceValue;
+
+                element.isExpanded = EditorGUILayout.Foldout(element.isExpanded, $"Edit {biomeData.name} Details", true);
+
+                if (element.isExpanded)
+                {
+                    EditorGUI.indentLevel++;
+                    EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+                    Editor cachedEditor = null;
+                    string key = biomeData.GetInstanceID().ToString();
+
+                    if (!_biomeEditors.TryGetValue(key, out cachedEditor))
+                    {
+                        cachedEditor = CreateEditor(biomeData);
+                        _biomeEditors[key] = cachedEditor;
+                    }
+
+                    if (cachedEditor != null && cachedEditor.target != null)
+                    {
+                        cachedEditor.OnInspectorGUI();
+                    }
+                    else
+                    {
+                        _biomeEditors.Remove(key);
+                    }
+
+                    EditorGUILayout.EndVertical();
+                    EditorGUI.indentLevel--;
+                }
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("Assign a BiomeData asset to configure it.", MessageType.Info);
+            }
+
+            EditorGUILayout.EndVertical();
+            GUILayout.Space(4);
+        }
+    }
+}
