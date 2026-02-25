7abf255d93dcae3ec9b2f05cb40bc1bf6f1d01a5
MrBytesized <williamlgale@gmail.com>
2026-02-25 01:21:00 +0000

    ﻿feat(runtime): add core MapGenerator class to execute and draw all GenModules.

--- DynamicDungeon_Tool/Assets/DynamicDungeon/Core/ThemeProfile.cs
+++ DynamicDungeon_Tool/Assets/DynamicDungeon/Core/ThemeProfile.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[CreateAssetMenu(menuName = "PCG/Theme Profile")]
+[CreateAssetMenu(menuName = "DynamicDungeon/Theme Profile")]
 public class ThemeProfile : ScriptableObject
 {
     [Serializable]
